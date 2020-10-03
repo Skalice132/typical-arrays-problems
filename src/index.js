b6e7970f6a688a2bb8a1b65fe88fcdf32f2930d7
Skalice132 <58006458+Skalice132@users.noreply.github.com>
2020-10-03 15:25:24 +0400

    feat: add min,max,avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minValue = 0;
+  if (array) {
+  	for(let i = 0; i < array.length; i++) {
+	    let currentValue = array[i];
+	    if(currentValue < minValue) {
+	        minValue = currentValue;
+	    }
+	  }
+	  return minValue;
+  } else {
+  	return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxValue = 0;
+  if (array) {
+  	for(let i = 0; i < array.length; i++) {
+    let currentValue = array[i];
+    if(currentValue > maxValue) {
+        maxValue = currentValue;
+    }
+  }
+  return maxValue;
+  } else {
+  	return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+		let sum = 0;
+	  for(let i = 0; i < array.length; i++) {
+	    let currentValue = array[i];
+	    sum += currentValue;
+	  }
+	  let result = sum/array.length;
+	  return result;		
 }
